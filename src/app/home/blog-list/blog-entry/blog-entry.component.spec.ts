e3c65e39dc2d70aeef3d6054473dfbfac3509e47
sebcon81 <Con>
2025-04-17 15:10:35 +0200

    adjust spec file

--- src/app/home/blog-list/blog-entry/blog-entry.component.spec.ts
+++ src/app/home/blog-list/blog-entry/blog-entry.component.spec.ts
@@ -23,27 +23,25 @@ describe("BlogEntryComponent", () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(BlogEntryComponent)
 		component = fixture.componentInstance
+		component.item = blogEntryItem
 		fixture.detectChanges()
 	})
 
 	it("should display h2 title", () => {
-		component.item = blogEntryItem
-		fixture.detectChanges()
+		// component.item = blogEntryItem
+		// fixture.detectChanges()
 		const h2 = fixture.debugElement.query(By.css("h2"))
 		expect(h2).toBeTruthy()
 		expect(h2.nativeElement.textContent).toContain(blogEntryItem.title)
 	})
 
 	it("should display img", () => {
-		component.item = blogEntryItem
-		fixture.detectChanges()
 		const img = fixture.debugElement.query(By.css("img"))
 		expect(img).toBeTruthy()
+		expect(img.nativeElement.getAttribute("src")).toBe(component.item.img)
 	})
 
 	it("should display p", () => {
-		component.item = blogEntryItem
-		fixture.detectChanges()
 		const p = fixture.debugElement.query(By.css("p"))
 		expect(p).toBeTruthy()
 	})
